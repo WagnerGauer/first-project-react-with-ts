015dd4671b3c16607ead45c3244c378ff5107d0f
wagner <wggauer@gmail.com>
2023-06-25 21:46:46 -0300

    learn that in order to use useRef with typescript I have to anotate the type of html element I am going to use it on and also that other possible value can be null and that I have to set null as a default valur for the ref and when I am using a property on the ref such as ref.current I have to do a check to see if ref is actually defined otherwise typescript complains

---
+++ src/refs/UserSearch.tsx
@@ -0,0 +1,52 @@
+import { useState, useRef, useEffect } from "react";
+
+const users = [
+  {
+    name: "Sarah",
+    age: 20,
+  },
+  { name: "John", age: 32 },
+  {
+    name: "Alex",
+    age: 24,
+  },
+];
+
+const UserSearch: React.FC = () => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [name, setName] = useState("");
+  const [user, setUser] = useState<{ name: string; age: number } | undefined>();
+
+  useEffect(() => {
+    if (!inputRef.current) {
+      return;
+    }
+    inputRef.current.focus();
+  }, []);
+
+  const onClick = () => {
+    const foundUser = users.find((user) => {
+      return user.name === name;
+    });
+
+    setUser(foundUser);
+  };
+
+  return (
+    <div>
+      User Search
+      <input
+        ref={inputRef}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={onClick}>Find User</button>
+      <div>{user?.name}</div>
+      {/* <div>{user&& user.name}</div> */}
+      {/* The first approach returns undefined and the second one returns null if the user is null*/}
+      {user?.age}
+    </div>
+  );
+};
+
+export default UserSearch;
